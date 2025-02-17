3e71576ddbfa5caef8b813802bdd75be87928199
Ayush Yadav <theayush434@gmail.com>
2025-02-17 22:06:23 +0530

    configure CORS settings for backend and add Vercel configuration file

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,13 @@ const cors = require('cors');
 
 
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        
+    }
+));
 
 app.use(express.json());
 
